e9f097a6c788800666ce771431bc918a0a637c05
KamleshPatel98 <kamleshp52170@gmail.com>
2024-10-23 00:04:26 +0530

    remove package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageCompressController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('image');
 });
+
+Route::post('image-save',[ImageCompressController::class,'imageCompress']);
